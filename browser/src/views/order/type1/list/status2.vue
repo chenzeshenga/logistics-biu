ab3ec68153083c16e7fb1ec791013e682b1e0207
chenzeshenga <chenzeshenga@163.com>
2019-03-16 18:02:50 +0800

    海外仓订单管理

--- browser/src/views/order/type1/list/status2.vue
+++ browser/src/views/order/type1/list/status2.vue
@@ -12,7 +12,7 @@
           <el-col :span="12">
             <el-table :data="tableData.row.contentList" stripe border>
               <el-table-column prop="sku" label="sku/东岳Sku" width="200"></el-table-column>
-              <el-table-column prop="name" label="商品名称"></el-table-column>
+              <el-table-column prop="name" label="商品名称" width="250"></el-table-column>
               <el-table-column prop="price" label="商品价格" width="180"></el-table-column>
               <el-table-column prop="num" label="商品数量" width="180"></el-table-column>
             </el-table>
