93ae3bc65eb5c8c1a2721249356c644ed5af365a
chenzeshenga <chenzeshenga@163.com>
2019-03-30 22:46:28 +0800

    商品批量审核

--- browser/src/views/order/type1/list/status1.vue
+++ browser/src/views/order/type1/list/status1.vue
@@ -1,111 +1,113 @@
 <template>
   <div class="login-container">
-    <el-col :offset="1" :span="20" style="margin-top: 10px;margin-bottom: 10px" class="block">
-      <el-button type="primary" @click="applyTrackNo()" style="margin-right: 10px" v-if="multiSelection">批量申请单号</el-button>
-      <el-button type="primary" @click="batchStatusUpdate()" style="margin-right: 50px" v-if="multiSelection">批量提交</el-button>
-      <el-date-picker v-model="daterange" type="daterange" align="right" unlink-panels range-separator="至" start-placeholder="开始日期"
-                      end-placeholder="结束日期" :picker-options="pickerOptions2" value-format="yyyy-MM-dd" style="width: 400px">
-      </el-date-picker>
-      <el-button icon="el-icon-search" @click="searchOrd()"></el-button>
-      <el-button type="primary" @click="route2NewOrd()" style="margin-left: 300px">新建订单</el-button>
-      <el-button type="primary" @click="exportExcel()" icon="iconfont icon-jichukongjiantubiao-gonggongxuanzekuang">导出excel</el-button>
-      <!--<el-button type="primary" @click="route2NewOrd()" icon="el-icon-tickets">导出csv</el-button>-->
-    </el-col>
-    <el-table style="width: 100%" :data="tableData" v-loading.body="tableLoading" element-loading-text="加载中" stripe
-              highlight-current-row @selection-change="handleSelectionChange">
-      <el-table-column type="selection" width="55"></el-table-column>
-      <el-table-column type="expand">
-        <template slot-scope="tableData">
-          <el-table :data="tableData.row.contentList" show-summary :summary-method="getSummary" style="margin-bottom: 0">
-            <el-table-column prop="sku" label="sku/东岳Sku" width="200"></el-table-column>
-            <el-table-column prop="name" label="商品名称" width="300"></el-table-column>
-            <el-table-column prop="price" label="商品价格(JPY)" width="180"></el-table-column>
-            <el-table-column prop="num" label="商品数量" width="180"></el-table-column>
-          </el-table>
-        </template>
-      </el-table-column>
-      <el-table-column width="160" prop="orderNo" label="订单号"></el-table-column>
-      <el-table-column width="150" prop="categoryName" label="订单类型"></el-table-column>
-      <el-table-column width="200" prop="channelDesc" label="运送渠道"></el-table-column>
-      <el-table-column width="100" prop="statusDesc" label="订单状态"></el-table-column>
-      <el-table-column width="150" prop="carrierName" label="承运人"></el-table-column>
-      <el-table-column width="170" prop="trackNo" label="追踪单号"></el-table-column>
-      <el-table-column width="250" prop="fromAddressDesc" label="寄件地址"></el-table-column>
-      <el-table-column width="150" prop="fromName" label="寄件人"></el-table-column>
-      <el-table-column width="150" prop="fromContact" label="寄件人联系方式"></el-table-column>
-      <el-table-column width="150" prop="fromZipCode" label="寄件人邮编"></el-table-column>
-      <el-table-column width="250" prop="toAddressDesc" label="收件地址"></el-table-column>
-      <el-table-column width="150" prop="toName" label="收件人"></el-table-column>
-      <el-table-column width="150" prop="toContact" label="收件人联系方式"></el-table-column>
-      <el-table-column width="150" prop="toZipCode" label="收件人邮编"></el-table-column>
-      <el-table-column width="80" prop="collect" label="是否代收商品费用"></el-table-column>
-      <el-table-column width="170" prop="createOn" label="创建时间"></el-table-column>
-      <el-table-column width="170" prop="updateOn" label="修改时间"></el-table-column>
-      <el-table-column width="150" prop="creator" label="创建人"></el-table-column>
-      <el-table-column width="150" prop="updator" label="修改人"></el-table-column>
-      <el-table-column label="操作" width="300" fixed="right">
-        <template slot-scope="scope">
-          <el-tooltip content="提交拣货" placement="top">
-            <el-button @click="statusUpdate(scope.$index,scope.row)" size="mini" type="info" icon="el-icon-check" circle
-                       plain></el-button>
-          </el-tooltip>
-          <el-tooltip content="申请单号" placement="top">
-            <el-button @click="applyTrackno(scope.$index,scope.row)" size="mini" type="info" icon="el-icon-info" circle plain></el-button>
-          </el-tooltip>
-          <el-tooltip content="编辑" placement="top">
-            <el-button @click="handleUpdate(scope.$index,scope.row)" size="mini" type="info" icon="el-icon-edit" circle plain></el-button>
-          </el-tooltip>
-          <el-tooltip content="打印配货单" placement="top">
-            <el-button @click="print(scope.$index,scope.row)" size="mini" type="info" icon="el-icon-printer" circle plain></el-button>
-          </el-tooltip>
-          <el-tooltip content="废弃" placement="top">
-            <el-button @click="abandon(scope.$index,scope.row)" size="mini" type="danger" icon="el-icon-remove" circle plain></el-button>
-          </el-tooltip>
-        </template>
-      </el-table-column>
-    </el-table>
-    <el-pagination style="margin-left: 65%;margin-top: 10px" background @size-change="handleSizeChange" @current-change="handleCurrentChange"
-                   :current-page="tablePage.current"
-                   :page-sizes="[10, 20, 30, 40, 50]" :page-size="tablePage.size" layout="total, sizes, prev, pager, next, jumper"
-                   :total="tablePage.total">
-    </el-pagination>
-    <!--:before-close="handleClose"-->
-    <el-dialog title="申请单号" :visible.sync="dialogVisible" width="30%">
-      <el-form :model="form">
-        <el-col :span="12">
-          <el-form-item label="承运人">
-            <el-cascader :options="carrier" v-model="form.selectedCarrier" @change="handleCarrierChange" filterable></el-cascader>
-          </el-form-item>
-        </el-col>
-        <el-col :span="24">
-          <el-form-item label="追踪单号">
-            <el-input v-model="form.trackNo" placeholder="您可点击按钮预先获取单号，后台操作员将根据实际情况进行调整">
-              <el-button slot="append" @click="getOrdNo2">获取单号</el-button>
-            </el-input>
-          </el-form-item>
-        </el-col>
-      </el-form>
-      <span slot="footer" class="dialog-footer">
+    <div class="app-container">
+      <el-col :offset="1" :span="20" style="margin-top: 10px;margin-bottom: 10px" class="block">
+        <el-button type="primary" @click="applyTrackNo()" style="margin-right: 10px" v-if="multiSelection">批量申请单号</el-button>
+        <el-button type="primary" @click="batchStatusUpdate()" style="margin-right: 50px" v-if="multiSelection">批量提交</el-button>
+        <el-date-picker v-model="daterange" type="daterange" align="right" unlink-panels range-separator="至" start-placeholder="开始日期"
+                        end-placeholder="结束日期" :picker-options="pickerOptions2" value-format="yyyy-MM-dd" style="width: 400px">
+        </el-date-picker>
+        <el-button icon="el-icon-search" @click="searchOrd()"></el-button>
+        <el-button type="primary" @click="route2NewOrd()" style="margin-left: 300px">新建订单</el-button>
+        <el-button type="primary" @click="exportExcel()" icon="iconfont icon-jichukongjiantubiao-gonggongxuanzekuang">导出excel</el-button>
+        <!--<el-button type="primary" @click="route2NewOrd()" icon="el-icon-tickets">导出csv</el-button>-->
+      </el-col>
+      <el-table style="width: 100%" :data="tableData" v-loading.body="tableLoading" element-loading-text="加载中" stripe
+                highlight-current-row @selection-change="handleSelectionChange">
+        <el-table-column type="selection" width="55"></el-table-column>
+        <el-table-column type="expand">
+          <template slot-scope="tableData">
+            <el-table :data="tableData.row.contentList" show-summary :summary-method="getSummary" style="margin-bottom: 0">
+              <el-table-column prop="sku" label="sku/东岳Sku" width="200"></el-table-column>
+              <el-table-column prop="name" label="商品名称" width="300"></el-table-column>
+              <el-table-column prop="price" label="商品价格(JPY)" width="180"></el-table-column>
+              <el-table-column prop="num" label="商品数量" width="180"></el-table-column>
+            </el-table>
+          </template>
+        </el-table-column>
+        <el-table-column width="160" prop="orderNo" label="订单号"></el-table-column>
+        <el-table-column width="150" prop="categoryName" label="订单类型"></el-table-column>
+        <el-table-column width="200" prop="channelDesc" label="运送渠道"></el-table-column>
+        <el-table-column width="100" prop="statusDesc" label="订单状态"></el-table-column>
+        <el-table-column width="150" prop="carrierName" label="承运人"></el-table-column>
+        <el-table-column width="170" prop="trackNo" label="追踪单号"></el-table-column>
+        <el-table-column width="250" prop="fromAddressDesc" label="寄件地址"></el-table-column>
+        <el-table-column width="150" prop="fromName" label="寄件人"></el-table-column>
+        <el-table-column width="150" prop="fromContact" label="寄件人联系方式"></el-table-column>
+        <el-table-column width="150" prop="fromZipCode" label="寄件人邮编"></el-table-column>
+        <el-table-column width="250" prop="toAddressDesc" label="收件地址"></el-table-column>
+        <el-table-column width="150" prop="toName" label="收件人"></el-table-column>
+        <el-table-column width="150" prop="toContact" label="收件人联系方式"></el-table-column>
+        <el-table-column width="150" prop="toZipCode" label="收件人邮编"></el-table-column>
+        <el-table-column width="80" prop="collect" label="是否代收商品费用"></el-table-column>
+        <el-table-column width="170" prop="createOn" label="创建时间"></el-table-column>
+        <el-table-column width="170" prop="updateOn" label="修改时间"></el-table-column>
+        <el-table-column width="150" prop="creator" label="创建人"></el-table-column>
+        <el-table-column width="150" prop="updator" label="修改人"></el-table-column>
+        <el-table-column label="操作" width="300" fixed="right">
+          <template slot-scope="scope">
+            <el-tooltip content="提交拣货" placement="top">
+              <el-button @click="statusUpdate(scope.$index,scope.row)" size="mini" type="info" icon="el-icon-check" circle
+                         plain></el-button>
+            </el-tooltip>
+            <el-tooltip content="申请单号" placement="top">
+              <el-button @click="applyTrackno(scope.$index,scope.row)" size="mini" type="info" icon="el-icon-info" circle plain></el-button>
+            </el-tooltip>
+            <el-tooltip content="编辑" placement="top">
+              <el-button @click="handleUpdate(scope.$index,scope.row)" size="mini" type="info" icon="el-icon-edit" circle plain></el-button>
+            </el-tooltip>
+            <el-tooltip content="打印配货单" placement="top">
+              <el-button @click="print(scope.$index,scope.row)" size="mini" type="info" icon="el-icon-printer" circle plain></el-button>
+            </el-tooltip>
+            <el-tooltip content="废弃" placement="top">
+              <el-button @click="abandon(scope.$index,scope.row)" size="mini" type="danger" icon="el-icon-remove" circle plain></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination style="margin-left: 65%;margin-top: 10px" background @size-change="handleSizeChange" @current-change="handleCurrentChange"
+                     :current-page="tablePage.current"
+                     :page-sizes="[10, 20, 30, 40, 50]" :page-size="tablePage.size" layout="total, sizes, prev, pager, next, jumper"
+                     :total="tablePage.total">
+      </el-pagination>
+      <!--:before-close="handleClose"-->
+      <el-dialog title="申请单号" :visible.sync="dialogVisible" width="30%">
+        <el-form :model="form">
+          <el-col :span="12">
+            <el-form-item label="承运人">
+              <el-cascader :options="carrier" v-model="form.selectedCarrier" @change="handleCarrierChange" filterable></el-cascader>
+            </el-form-item>
+          </el-col>
+          <el-col :span="24">
+            <el-form-item label="追踪单号">
+              <el-input v-model="form.trackNo" placeholder="您可点击按钮预先获取单号，后台操作员将根据实际情况进行调整">
+                <el-button slot="append" @click="getOrdNo2">获取单号</el-button>
+              </el-input>
+            </el-form-item>
+          </el-col>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
                 <el-button @click="dialogVisible = false">取 消</el-button>
                 <el-button type="primary" @click="fillInTrackNo">确 定</el-button>
             </span>
-    </el-dialog>
-    <el-dialog title="批量申请单号" :visible.sync="dialogVisibleList" width="30%">
-      <div style="margin-bottom: 20px">
-        <span>请选择承运人,系统将自动分配追踪单号</span>
-      </div>
-      <el-form :model="form">
-        <el-col :span="24">
-          <el-form-item label="承运人">
-            <el-cascader :options="carrier" v-model="form.selectedCarrier" @change="handleCarrierChange" filterable></el-cascader>
-          </el-form-item>
-        </el-col>
-      </el-form>
-      <span slot="footer" class="dialog-footer">
+      </el-dialog>
+      <el-dialog title="批量申请单号" :visible.sync="dialogVisibleList" width="30%">
+        <div style="margin-bottom: 20px">
+          <span>请选择承运人,系统将自动分配追踪单号</span>
+        </div>
+        <el-form :model="form">
+          <el-col :span="24">
+            <el-form-item label="承运人">
+              <el-cascader :options="carrier" v-model="form.selectedCarrier" @change="handleCarrierChange" filterable></el-cascader>
+            </el-form-item>
+          </el-col>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
                 <el-button @click="dialogVisibleList = false">取 消</el-button>
                 <el-button type="primary" @click="fillInTrackNoList">确 定</el-button>
             </span>
-    </el-dialog>
+      </el-dialog>
+    </div>
   </div>
 </template>
 
@@ -280,7 +282,7 @@
         this.form.orderNo = row.orderNo;
         this.form.carrierNo = row.carrierNo;
         this.form.trackNo = row.trackNo;
-        let tmpNo = [];
+        const tmpNo = [];
         tmpNo.push('carrier_' + row.carrierNo);
         this.form.selectedCarrier = tmpNo;
         this.dialogVisible = true;
--- browser/src/views/product/mgt/status0.vue
+++ browser/src/views/product/mgt/status0.vue
@@ -1,49 +1,51 @@
 <template>
   <div class="login-container">
-    <el-table style="width: 100%;margin: 10px" :data="tableData" v-loading.body="tableLoading"
-              element-loading-text="加载中" stripe
-              highlight-current-row>
-      <el-table-column type="selection" width="55"></el-table-column>
-      <el-table-column width="150" prop="sku" label="sku"></el-table-column>
-      <el-table-column width="150" prop="dySku" label="东岳sku"></el-table-column>
-      <el-table-column width="150" prop="statusDesc" label="状态"></el-table-column>
-      <el-table-column width="200" prop="productName" label="商品名称"></el-table-column>
-      <el-table-column width="150" prop="categoryName" label="商品类型"></el-table-column>
-      <el-table-column prop="color" label="商品颜色" width="150"></el-table-column>
-      <el-table-column prop="size" label="商品尺寸" width="150"></el-table-column>
-      <el-table-column prop="length" label="商品长度(cm)" width="150"></el-table-column>
-      <el-table-column prop="width" label="商品宽度(cm)" width="150"></el-table-column>
-      <el-table-column prop="height" label="商品高度(cm)" width="150"></el-table-column>
-      <el-table-column prop="weight" label="商品重量(kg)" width="150"></el-table-column>
-      <el-table-column prop="price" label="商品价格" width="150"></el-table-column>
-      <el-table-column prop="createdBy" label="创建者" width="150"></el-table-column>
-      <el-table-column prop="updateBy" label="更新者" width="150"></el-table-column>
-      <el-table-column prop="createOn" label="创建时间" width="160"></el-table-column>
-      <el-table-column prop="updateOn" label="更新时间" width="160"></el-table-column>
-      <el-table-column label="操作" width="200" fixed="right">
-        <template slot-scope="scope">
-          <el-tooltip content="审核" placement="top">
-            <el-button @click="statusUpdate(scope.$index,scope.row)" size="mini" type="info"
-                       icon="el-icon-check" circle
-                       plain></el-button>
-          </el-tooltip>
-          <el-tooltip content="编辑" placement="top">
-            <el-button @click="handleUpdate(scope.$index,scope.row)" size="mini" type="info"
-                       icon="el-icon-edit" circle plain></el-button>
-          </el-tooltip>
-          <el-tooltip content="删除" placement="top">
-            <el-button @click="handleDelete(scope.$index,scope.row)" size="mini" type="danger"
-                       icon="el-icon-remove" circle plain></el-button>
-          </el-tooltip>
-        </template>
-      </el-table-column>
-    </el-table>
-    <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
-                   :current-page="tablePage.current"
-                   :page-sizes="[10, 20, 30, 40, 50]" :page-size="tablePage.size"
-                   layout="total, sizes, prev, pager, next, jumper"
-                   :total="tablePage.total">
-    </el-pagination>
+    <div class="app-container">
+      <el-table style="width: 100%;margin: 10px" :data="tableData" v-loading.body="tableLoading"
+                element-loading-text="加载中" stripe
+                highlight-current-row @selection-change="handleSelectionChange">
+        <el-table-column type="selection" width="55"></el-table-column>
+        <el-table-column width="150" prop="sku" label="sku"></el-table-column>
+        <el-table-column width="150" prop="dySku" label="东岳sku"></el-table-column>
+        <el-table-column width="150" prop="statusDesc" label="状态"></el-table-column>
+        <el-table-column width="200" prop="productName" label="商品名称"></el-table-column>
+        <el-table-column width="150" prop="categoryName" label="商品类型"></el-table-column>
+        <el-table-column prop="color" label="商品颜色" width="150"></el-table-column>
+        <el-table-column prop="size" label="商品尺寸" width="150"></el-table-column>
+        <el-table-column prop="length" label="商品长度(cm)" width="150"></el-table-column>
+        <el-table-column prop="width" label="商品宽度(cm)" width="150"></el-table-column>
+        <el-table-column prop="height" label="商品高度(cm)" width="150"></el-table-column>
+        <el-table-column prop="weight" label="商品重量(kg)" width="150"></el-table-column>
+        <el-table-column prop="price" label="商品价格" width="150"></el-table-column>
+        <el-table-column prop="createdBy" label="创建者" width="150"></el-table-column>
+        <el-table-column prop="updateBy" label="更新者" width="150"></el-table-column>
+        <el-table-column prop="createOn" label="创建时间" width="160"></el-table-column>
+        <el-table-column prop="updateOn" label="更新时间" width="160"></el-table-column>
+        <el-table-column label="操作" width="200" fixed="right">
+          <template slot-scope="scope">
+            <el-tooltip content="审核" placement="top">
+              <el-button @click="statusUpdate(scope.$index,scope.row)" size="mini" type="info"
+                         icon="el-icon-check" circle
+                         plain></el-button>
+            </el-tooltip>
+            <el-tooltip content="编辑" placement="top">
+              <el-button @click="handleUpdate(scope.$index,scope.row)" size="mini" type="info"
+                         icon="el-icon-edit" circle plain></el-button>
+            </el-tooltip>
+            <el-tooltip content="删除" placement="top">
+              <el-button @click="handleDelete(scope.$index,scope.row)" size="mini" type="danger"
+                         icon="el-icon-remove" circle plain></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
+                     :current-page="tablePage.current"
+                     :page-sizes="[10, 20, 30, 40, 50]" :page-size="tablePage.size"
+                     layout="total, sizes, prev, pager, next, jumper"
+                     :total="tablePage.total">
+      </el-pagination>
+    </div>
   </div>
 </template>
 
@@ -120,6 +122,9 @@
       },
       handleUpdate(index, row) {
         this.$router.push({path: '/new-product/new-product?sku=' + row.sku});
+      },
+      handleSelectionChange(val) {
+        console.log(val);
       },
     },
   };
