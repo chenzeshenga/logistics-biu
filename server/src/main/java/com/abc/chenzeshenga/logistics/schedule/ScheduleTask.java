7ee03a9c9f37f66a80d909b7bf3beac1d3aaed85
chenzeshenga <chenzeshenga@163.com>
2019-04-24 23:39:23 +0800

    定时任务，统计已审核商品的总数

--- server/src/main/java/com/abc/chenzeshenga/logistics/schedule/ScheduleTask.java
+++ server/src/main/java/com/abc/chenzeshenga/logistics/schedule/ScheduleTask.java
@@ -1,10 +1,13 @@
 package com.abc.chenzeshenga.logistics.schedule;
 
 import com.abc.chenzeshenga.logistics.mapper.OrderMapper;
+import com.abc.chenzeshenga.logistics.mapper.ProductMapper;
 import com.abc.chenzeshenga.logistics.mapper.ProductStatisticsMapper;
 import com.abc.chenzeshenga.logistics.model.ManualOrder;
 import com.abc.chenzeshenga.logistics.model.ManualOrderContent;
+import com.abc.chenzeshenga.logistics.model.Product;
 import com.abc.chenzeshenga.logistics.model.ProductStatistics;
+import com.baomidou.mybatisplus.plugins.Page;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -22,11 +25,23 @@ import java.util.*;
 
     @Resource private ProductStatisticsMapper productStatisticsMapper;
 
+    @Resource private ProductMapper productMapper;
+
     @Scheduled(fixedDelay = 120000) public void countProduct() {
         log.info("scheduled task countProduct begin");
         List<ProductStatistics> productStatisticsList = new ArrayList<>();
         List<ManualOrder> manualOrderList = orderMapper.listAll();
+        List<Product> productList = productMapper.listByStatus(new Page(0, 999), "1");
         Map<String, ProductStatistics> productStatisticsMap = new HashMap<>(12);
+        for (Product product : productList) {
+            ProductStatistics productStatistics = new ProductStatistics();
+            productStatistics.setSku(product.getSku());
+            productStatistics.setDysku(product.getDySku());
+            productStatistics.setOwner(product.getCreatedBy());
+            productStatistics.setTotalNum(product.getAvaNum());
+            productStatisticsMap
+                .put(product.getSku() + "/" + product.getDySku() + ":" + product.getCreatedBy(), productStatistics);
+        }
         manualOrderList.forEach(manualOrder -> {
             if ("1".equals(manualOrder.getCategory()) && "2".equals(manualOrder.getStatus())) {
                 List<ManualOrderContent> manualOrderContentList = manualOrder.getManualOrderContents();
@@ -42,16 +57,20 @@ import java.util.*;
                             productStatistics.setDysku(sku.split("/")[1]);
                             productStatistics.setOwner(owner);
                             productStatistics.setStatisticalTime(new Date());
-                            productStatistics.setPendingPickupNum(
-                                String.valueOf(Integer.valueOf(manualOrderContent.getNum()) - Integer.valueOf(manualOrderContent.getPicked())));
-                            productStatistics.setPendingDeliverNum(String.valueOf(Integer.valueOf(manualOrderContent.getPicked())));
+                            productStatistics.setPendingPickupNum(String.valueOf(
+                                Integer.valueOf(manualOrderContent.getNum()) - Integer
+                                    .valueOf(manualOrderContent.getPicked())));
+                            productStatistics
+                                .setPendingDeliverNum(String.valueOf(Integer.valueOf(manualOrderContent.getPicked())));
                         } else {
                             productStatistics.setStatisticalTime(new Date());
                             productStatistics.setPendingPickupNum(String.valueOf(
-                                Integer.valueOf(manualOrderContent.getNum()) - Integer.valueOf(manualOrderContent.getPicked()) + Integer
+                                Integer.valueOf(manualOrderContent.getNum()) - Integer
+                                    .valueOf(manualOrderContent.getPicked()) + Integer
                                     .valueOf(productStatistics.getPendingPickupNum())));
                             productStatistics.setPendingDeliverNum(String.valueOf(Integer.valueOf(
-                                Integer.valueOf(manualOrderContent.getPicked()) + Integer.valueOf(productStatistics.getPendingDeliverNum()))));
+                                Integer.valueOf(manualOrderContent.getPicked()) + Integer
+                                    .valueOf(productStatistics.getPendingDeliverNum()))));
                         }
                         productStatisticsMap.put(key, productStatistics);
                     });
