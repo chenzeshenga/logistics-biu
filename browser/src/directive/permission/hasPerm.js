fa5d2a0c6a1bf041d4220af6bd6885fa974b185f
chenzeshenga <chenzeshenga@163.com>
2019-07-23 00:39:40 +0800

    :sparkles: 阅读权限部分源码

--- browser/src/directive/permission/hasPerm.js
+++ browser/src/directive/permission/hasPerm.js
@@ -2,11 +2,14 @@
 import store from '@/store'
 
 export default function(permStr) {
-  // 如果传入的权限字符串值是空的，视为有权限
-  if(!permStr || permStr=='') return true;
-  // 超级管理员权限
-  if(store.getters.perms.some(p=>p.val=="*")) return true;
-  // 校验权限
-  return store.getters.perms.some(p=>p.val==permStr)
+    // 如果传入的权限字符串值是空的，视为有权限
+    if (!permStr || permStr == '') {
+        return true
+    }
+    // 超级管理员权限
+    if (store.getters.perms.some(p => p.val == '*')) {
+        return true
+    }
+    // 校验权限
+    return store.getters.perms.some(p => p.val == permStr)
 }
-
