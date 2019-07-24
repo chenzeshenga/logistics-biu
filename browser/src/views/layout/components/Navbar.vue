10ec75ed127bffafe17a94599929ea11440e7132
chenzeshenga <chenzeshenga@163.com>
2019-07-25 00:27:51 +0800

    :bookmark: 版本升级

--- browser/src/views/layout/components/Navbar.vue
+++ browser/src/views/layout/components/Navbar.vue
@@ -223,7 +223,7 @@ export default {
             dialogVisible: false,
             dialogVisible2: false,
             dialogVisible3: false,
-            version: '20190721',
+            version: '20190725',
             temp: {
                 pwd: null,
                 pwd2: null,
