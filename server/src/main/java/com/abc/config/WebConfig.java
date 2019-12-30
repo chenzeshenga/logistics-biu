d06655ab4aca997d327674d06a0c9a3e7b47d25e
chenzeshenga <chenzeshenga@163.com>
2019-12-30 23:33:03 +0800

    format

--- server/src/main/java/com/abc/config/WebConfig.java
+++ server/src/main/java/com/abc/config/WebConfig.java
@@ -8,14 +8,11 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static com.alibaba.fastjson.serializer.SerializerFeature.*;
-
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -23,25 +20,24 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
-        //序列化配置
+        // 序列化配置
         FastJsonConfig config = new FastJsonConfig();
-        config.setSerializerFeatures(
-                QuoteFieldNames,// 输出key时是否使用双引号
-                WriteMapNullValue,// 是否输出值为null的字段
-                WriteNullNumberAsZero,//数值字段如果为null,输出为0,而非null
-                WriteNullListAsEmpty,//List字段如果为null,输出为[],而非null
-                WriteNullStringAsEmpty,//字符类型字段如果为null,输出为"",而非null
-                //WriteNullBooleanAsFalse,//Boolean字段如果为null,输出为false,而非null
-                //WriteNullStringAsEmpty,// null String不输出
-                //WriteMapNullValue,//null String也要输出
-                //WriteDateUseDateFormat,//Date的日期转换器
-                DisableCircularReferenceDetect//禁止循环引用
+        config.setSerializerFeatures(QuoteFieldNames, // 输出key时是否使用双引号
+                WriteMapNullValue, // 是否输出值为null的字段
+                WriteNullNumberAsZero, // 数值字段如果为null,输出为0,而非null
+                WriteNullListAsEmpty, // List字段如果为null,输出为[],而非null
+                WriteNullStringAsEmpty, // 字符类型字段如果为null,输出为"",而非null
+                // WriteNullBooleanAsFalse,//Boolean字段如果为null,输出为false,而非null
+                // WriteNullStringAsEmpty,// null String不输出
+                // WriteMapNullValue,//null String也要输出
+                // WriteDateUseDateFormat,//Date的日期转换器
+                DisableCircularReferenceDetect// 禁止循环引用
         );
         converter.setFastJsonConfig(config);
         converters.add(converter);
     }
 
-    //解决跨域
+    // 解决跨域
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration conf = new CorsConfiguration();
@@ -60,6 +56,5 @@ public class WebConfig implements WebMvcConfigurer {
         source.registerCorsConfiguration("/**", conf); // 4 对接口配置跨域设置
         return new CorsFilter(source);
     }
-
 
 }
