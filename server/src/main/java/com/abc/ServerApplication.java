f3129fcaadaf5fb90b0863e825937f220ea935fe
chenzesheng <chenzesheng@cpic.com.cn>
2020-01-26 16:24:40 +0800

    format

--- server/src/main/java/com/abc/ServerApplication.java
+++ server/src/main/java/com/abc/ServerApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-/** created by CaiBaoHong at 2018/4/17 14:15<br> */
-@SpringBootApplication(scanBasePackages = {"com.abc"})
+/**
+ * created by CaiBaoHong at 2018/4/17 14:15<br>
+ */
+@SpringBootApplication(scanBasePackages = { "com.abc" })
 @EnableScheduling
 public class ServerApplication {
 
