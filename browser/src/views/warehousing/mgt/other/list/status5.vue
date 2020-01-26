25e010da71344dd4d022d454967219199480c941
chenzesheng <chenzesheng@cpic.com.cn>
2020-01-26 15:04:35 +0800

    入库单管理更新

---
+++ browser/src/views/warehousing/mgt/other/list/status5.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="login-container">
+    <div class="app-container">
+      <warehousing-table v-bind:msg="this.msg"></warehousing-table>
+    </div>
+  </div>
+</template>
+
+<script>
+import WarehousingTable from "../../../../../components/dy/WarehousingTable/index";
+
+export default {
+  name: "warehousing-mgt-other-list-status5",
+  components: { WarehousingTable },
+  data() {
+    return {
+      msg: {
+        status: "5",
+        category: "2",
+        buttonVisible1: true,
+        buttonVisible6: true,
+        buttonVisible7: true,
+      }
+    };
+  }
+};
+</script>
