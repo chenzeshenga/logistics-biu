60a2c590cbd7cc069fe87f4c5a459c3cd274cc4a
chenzesheng <chenzesheng@cpic.com.cn>
2020-01-29 22:39:31 +0800

    init mapper

---
+++ server/src/main/java/com/abc/chenzeshenga/logistics/mapper/user/UserRoleMapper.java
@@ -0,0 +1,15 @@
+package com.abc.chenzeshenga.logistics.mapper.user;
+
+import com.abc.chenzeshenga.logistics.model.user.UserRoleMapping;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author chenzeshenga
+ * @since 2020-01-29 22:38
+ */
+@Mapper
+public interface UserRoleMapper extends BaseMapper<UserRoleMapping> {
+
+}
