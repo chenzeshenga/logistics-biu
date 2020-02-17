4fe228305db53ab7acd70eefff2c8fcac5c3f2a5
chenzeshenga <chenzeshenga@163.com>
2020-02-17 21:28:33 +0800

    vuepress update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,17 @@
 module.exports = {
   title: '东岳用户手册',
   description: '东岳用户手册',
-  base: "/docs"
+  base: "/docs",
+  themeConfig: {
+    lastUpdated: '上一次更新',
+    nav: [
+      {text: '用户指南', link: '/'},
+      {text: '主页', link: 'http://47.105.33.48/'},
+    ],
+    sidebar: [
+      '/',
+      '/page-a',
+      ['/page-b', 'Explicit link text']
+    ]
+  }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,9 +1,5 @@
-'# Hello VuePress!' 
-
-
 ---
 home: true
-heroImage: /hero.png
 heroText: Hero 标题
 tagline: Hero 副标题
 actionText: 快速上手 →
@@ -17,3 +13,7 @@ features:
   details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
 footer: MIT Licensed | Copyright © 2018-present Evan You
 ---
+
+'# Hello VuePress!' 
+
+
