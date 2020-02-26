acac0bc56c872e778ffe84c9288cee9fe8237faf
chenzeshenga <chenzeshenga@163.com>
2020-02-26 23:32:35 +0800

    product barcode style update

--- browser/src/components/@syncfusion/syncfusion-barcode-product.vue
+++ browser/src/components/@syncfusion/syncfusion-barcode-product.vue
@@ -1,14 +1,16 @@
 <template>
-  <ejs-barcodegenerator
-      id="barcode"
+  <div style="width: 145px;height: 80px">
+    <span>{{name}}</span>
+    <ejs-barcodegenerator
+      id="barcode4product"
       ref="barcodeControl"
       :width="width"
       :height="height"
       :type="type"
       :value="value"
       :mode="mode"
-      style="margin:9%"
-  />
+    />
+  </div>
 </template>
 
 <script>
@@ -19,26 +21,29 @@ Vue.use(BarcodeGeneratorPlugin);
 let barCodeInstance;
 
 export default {
-  name: 'customSyncfusionBarcode4ShelfNo',
+  name: 'syncfusion-barcode-product',
   props: ['barcode'],
   data: function() {
     return {
-      width: this.barcode.width,
-      height: this.barcode.height,
       mode: 'SVG',
       type: 'Code128',
+      width: 145,
+      height: 60,
       value: this.barcode.value,
       fontcolorvalue: '#000',
       textPositionwidth: '100%',
       ftextPositionfields: {value: 'type', text: 'text'},
       textPositiondataSource: {type: 'Bottom', text: 'Bottom'},
       textAlignmentdataSource: {type: 'Center', text: 'Center'},
+      name: '我我我',
     };
   },
-  mounted: function() {
+  mounted() {
     barCodeInstance = this.$refs.barcodeControl.ej2Instances;
-    if (this.barcode.textSize) {
-      barCodeInstance.displayText.size = this.barcode.textSize;
+    if (this.barcode.value) {
+      barCodeInstance.displayText.text = this.barcode.value;
+      barCodeInstance.margin.top = 2;
+      barCodeInstance.margin.bottom = 2;
     }
   },
 };
