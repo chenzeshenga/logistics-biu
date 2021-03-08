ac79b150bdce003982acbbe5b809f83e217f7e03
chenzeshenga <chenzeshenga@163.com>
2021-03-08 15:01:39 +0800

    增加费用实体类

---
+++ server/src/main/java/com/abc/chenzeshenga/logistics/model/fee/CustomerFee.java
@@ -0,0 +1,39 @@
+package com.abc.chenzeshenga.logistics.model.fee;
+
+import java.math.BigDecimal;
+
+/** @author chenzeshenga */
+public class CustomerFee {
+
+  /**
+   * 用户id
+   */
+  private String userId;
+  /**
+   * 费用期间
+   */
+  private String relatedMonth;
+  /**
+   * 货币
+   */
+  private String currency;
+
+  /**
+   * 入库查询-接收退货手续费
+   */
+  private BigDecimal fee1;
+  /**
+   *
+   */
+  private BigDecimal fee2;
+  private BigDecimal fee3;
+  private BigDecimal fee4;
+  private BigDecimal fee5;
+  private BigDecimal fee6;
+  private BigDecimal fee7;
+  private BigDecimal fee8;
+  private BigDecimal fee9;
+  private BigDecimal fee10;
+
+  private String comments;
+}
